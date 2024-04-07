359ece81a03ce41c337f9a8d72c53f97260f7e76
AsyncMatrix <Catbob614@gmail.com>
2024-04-07 22:51:37 +0100

    feature: update and implement Component buttons/labels

--- Src/Interface/Components/TextButton.lua
+++ Src/Interface/Components/TextButton.lua
@@ -3,17 +3,16 @@ local Sift = require(script.Parent.Parent.Parent.Packages.Sift)
 
 local InterfaceTheme = require(script.Parent.Parent.Parent.Data.InterfaceTheme)
 
-local function TextLabel(props)
-	return React.createElement("TextLabel", Sift.Dictionary.merge({
+local function TextButton(props)
+	return React.createElement("TextButton", Sift.Dictionary.merge({
 		FontFace = InterfaceTheme.TextFontItalic,
 		TextColor3 = InterfaceTheme.Secondary.White,
 		TextScaled = true,
-		TextWrapped = true,
 		Size = UDim2.fromScale(1, 1),
-		BackgroundTransparency = 1,
+		BackgroundTransparency = 0.5,
 		Position = UDim2.fromScale(0.5, 0.5),
 		AnchorPoint = Vector2.new(0.5, 0.5),
 	}, props))
 end
 
-return TextLabel
+return TextButton
