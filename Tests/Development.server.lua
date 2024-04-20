aaf8e878bcf96a44dc9eca951cca4b7a615d87ed
AsyncMatrix <Catbob614@gmail.com>
2024-04-20 10:51:46 +0100

    ENG-91: demo the Action in the 'Development' server script

--- Tests/Development.server.lua
+++ Tests/Development.server.lua
@@ -7,7 +7,25 @@ MetrikSDK.Server:SetProjectId(ServerStorage.__Project_Id.Value)
 MetrikSDK.Server:SetAuthenticationToken(ServerStorage.__Project_Auth.Value)
 
 MetrikSDK.Server:InitializeAsync():andThen(function()
-	-- warn("Metrik SDK loaded!")
+	warn("Metrik SDK loaded!")
+	
+	local action = MetrikSDK.Server.ActionBuilder.new()
+		:SetName("Hello, world!")
+		:SetDescription("A simple 'Hello, World' print!")
+		:AddArgument("String", {
+			Type = "string",
+			Required = false,
+			Description = "Anything else you want to print?"
+		})
+		:Build()
+
+	function action:OnRun(source: string?)
+		print("Hello, World!")
+
+		if source then
+			print(source)
+		end
+	end
 end):catch(function(exception)
-	-- warn("Metrik SDK failed: ", exception)
+	warn("Metrik SDK failed: ", exception)
 end)
